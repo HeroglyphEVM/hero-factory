540f02f04611abfa2ccbb1f477e9e9f6f0692ba3
Terkix <terkix.eth@proton.me>
2025-02-13 12:16:00 -0800

    refac: Changed the 'app.config' file to use mainnet instead of sepolia. Other small style changes. (#1)

--- app.config.ts
+++ app.config.ts
@@ -11,11 +11,11 @@ export type AppConfig = {
 
 const appConfig = {
   // The network where your DApp lives in
-  targetNetwork: chains.baseSepolia,
+  targetNetwork: chains.mainnet,
   // targetNetworks: [chains.baseSepolia],
 
   // The networks where your DApp can be connected to
-  chains: [chains.baseSepolia],
+  chains: [chains.mainnet],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect on the local network
@@ -25,14 +25,17 @@ const appConfig = {
   // You can get your own at https://dashboard.alchemyapi.io
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
-  alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "rEjtgJdffu-YuX-yux6nhRYH5NFrllnO",
+  alchemyApiKey:
+    process.env.NEXT_PUBLIC_ALCHEMY_API_KEY ||
+    "rEjtgJdffu-YuX-yux6nhRYH5NFrllnO",
 
   // This is ours WalletConnect's default project ID.
   // You can get your own at https://cloud.walletconnect.com
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
-  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "e4c944d25e0008d53a376056bef70c8d",
-
+  walletConnectProjectId:
+    process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ||
+    "e4c944d25e0008d53a376056bef70c8d",
 } satisfies AppConfig;
 
 export default appConfig;
