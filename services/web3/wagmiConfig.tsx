b8c7f3affa680809dfea5d1a86826b83404487b7
Terkix <terkix.eth@proton.me>
2025-08-25 16:17:22 +0800

    refac: Changed the RPC back to use Alchemy because Envio was not working properly

--- services/web3/wagmiConfig.tsx
+++ services/web3/wagmiConfig.tsx
@@ -2,7 +2,7 @@ import { hardhat } from "viem/chains";
 import { Chain, createClient, http } from "viem";
 import { wagmiConnectors } from "./wagmiConnectors";
 import { createConfig } from "wagmi";
-import { getEnvioRpcUrl } from "@/utils/web3/networks";
+import { getRpcHttpUrl } from "@/utils/web3/networks";
 import appConfig from "@/app.config";
 
 export const wagmiConfig = createConfig({
@@ -11,18 +11,17 @@ export const wagmiConfig = createConfig({
   ssr: true,
   client({ chain }) {
     // Return example: https://base-sepolia.rpc.hypersync.xyz/<token>
-    const url = getEnvioRpcUrl(chain.id);
+    // const url = getEnvioRpcUrl(chain.id);
+    const url = getRpcHttpUrl(chain.id);
 
     if (!url) {
       throw new Error(`No RPC URL found for chain ${chain.id}`);
     }
 
-    // TODO: MODIFY BELOW - Right now I will be using the base sepolia rpc url but I need to change this later to use the hypersync rpc url if I can make it work.
-
     return createClient({
       chain,
-      transport: http("https://sepolia.base.org"),
-      // transport: http(url),
+      // transport: http("https://sepolia.base.org"),
+      transport: http(url),
       ...(chain.id !== (hardhat as Chain).id
         ? {
             pollingInterval: appConfig.pollingInterval,
--- utils/web3/networks.ts
+++ utils/web3/networks.ts
@@ -2,68 +2,64 @@ import appConfig from "@/app.config";
 import * as chains from "viem/chains";
 
 // Mapping of chainId to RPC chain name an format followed by Envio
-export const RPC_CHAIN_NAMES: Record<number, string> = {
-  [chains.mainnet.id]: "eth",
-  [chains.sepolia.id]: "sepolia",
-  [chains.optimism.id]: "optimism",
-  [chains.optimismGoerli.id]: "420",
-  [chains.optimismSepolia.id]: "optimism-sepolia",
-  [chains.arbitrum.id]: "arbitrum",
-  [chains.arbitrumGoerli.id]: "421613",
-  [chains.arbitrumSepolia.id]: "arbitrum-sepolia",
-  [chains.polygon.id]: "polygon",
-  [chains.polygonMumbai.id]: "80001",
-  [chains.polygonAmoy.id]: "polygon-amoy",
-  [chains.astar.id]: "astar",
-  [chains.polygonZkEvm.id]: "polygon-zkevm",
-  [chains.polygonZkEvmTestnet.id]: "1442",
-  [chains.base.id]: "base",
-  [chains.baseGoerli.id]: "84531",
-  [chains.baseSepolia.id]: "base-sepolia",
-};
-
-export const getEnvioRpcUrl = (chainId: number) => {
-  const name = RPC_CHAIN_NAMES[chainId];
-  if (!name) return undefined;
-
-  const urlBase = `https://${name}.rpc.hypersync.xyz`;
-  const envioToken = appConfig.hypersyncBearerToken;
-
-  return envioToken ? `${urlBase}/${envioToken}` : urlBase;
-};
-
-// ! The code below is for Alchemy, use if we need to switch back to it.
-
 // export const RPC_CHAIN_NAMES: Record<number, string> = {
-//   [chains.mainnet.id]: "eth-mainnet",
-//   [chains.goerli.id]: "eth-goerli",
-//   [chains.sepolia.id]: "eth-sepolia",
-//   [chains.optimism.id]: "opt-mainnet",
-//   [chains.optimismGoerli.id]: "opt-goerli",
-//   [chains.optimismSepolia.id]: "opt-sepolia",
-//   [chains.arbitrum.id]: "arb-mainnet",
-//   [chains.arbitrumGoerli.id]: "arb-goerli",
-//   [chains.arbitrumSepolia.id]: "arb-sepolia",
-//   [chains.polygon.id]: "polygon-mainnet",
-//   [chains.polygonMumbai.id]: "polygon-mumbai",
+//   [chains.mainnet.id]: "eth",
+//   [chains.sepolia.id]: "sepolia",
+//   [chains.optimism.id]: "optimism",
+//   [chains.optimismGoerli.id]: "420",
+//   [chains.optimismSepolia.id]: "optimism-sepolia",
+//   [chains.arbitrum.id]: "arbitrum",
+//   [chains.arbitrumGoerli.id]: "421613",
+//   [chains.arbitrumSepolia.id]: "arbitrum-sepolia",
+//   [chains.polygon.id]: "polygon",
+//   [chains.polygonMumbai.id]: "80001",
 //   [chains.polygonAmoy.id]: "polygon-amoy",
-//   [chains.astar.id]: "astar-mainnet",
-//   [chains.polygonZkEvm.id]: "polygonzkevm-mainnet",
-//   [chains.polygonZkEvmTestnet.id]: "polygonzkevm-testnet",
-//   [chains.base.id]: "base-mainnet",
-//   [chains.baseGoerli.id]: "base-goerli",
+//   [chains.astar.id]: "astar",
+//   [chains.polygonZkEvm.id]: "polygon-zkevm",
+//   [chains.polygonZkEvmTestnet.id]: "1442",
+//   [chains.base.id]: "base",
+//   [chains.baseGoerli.id]: "84531",
 //   [chains.baseSepolia.id]: "base-sepolia",
 // };
 
-// export const getRpcHttpUrl = (chainId: number) => {
-//   console.log(
-//     `https://${RPC_CHAIN_NAMES[chainId]}.g.alchemy.com/v2/${appConfig.alchemyApiKey}`
-//   );
+// export const getEnvioRpcUrl = (chainId: number) => {
+//   const name = RPC_CHAIN_NAMES[chainId];
+//   if (!name) return undefined;
 
-//   return RPC_CHAIN_NAMES[chainId]
-//     ? `https://${RPC_CHAIN_NAMES[chainId]}.g.alchemy.com/v2/${appConfig.alchemyApiKey}`
-//     : undefined;
+//   const urlBase = `https://${name}.rpc.hypersync.xyz`;
+//   const envioToken = appConfig.hypersyncBearerToken;
+
+//   return envioToken ? `${urlBase}/${envioToken}` : urlBase;
 // };
+
+// ! The code below is for Alchemy, use if we need to switch back to it.
+
+export const RPC_CHAIN_NAMES: Record<number, string> = {
+  [chains.mainnet.id]: "eth-mainnet",
+  [chains.goerli.id]: "eth-goerli",
+  [chains.sepolia.id]: "eth-sepolia",
+  [chains.optimism.id]: "opt-mainnet",
+  [chains.optimismGoerli.id]: "opt-goerli",
+  [chains.optimismSepolia.id]: "opt-sepolia",
+  [chains.arbitrum.id]: "arb-mainnet",
+  [chains.arbitrumGoerli.id]: "arb-goerli",
+  [chains.arbitrumSepolia.id]: "arb-sepolia",
+  [chains.polygon.id]: "polygon-mainnet",
+  [chains.polygonMumbai.id]: "polygon-mumbai",
+  [chains.polygonAmoy.id]: "polygon-amoy",
+  [chains.astar.id]: "astar-mainnet",
+  [chains.polygonZkEvm.id]: "polygonzkevm-mainnet",
+  [chains.polygonZkEvmTestnet.id]: "polygonzkevm-testnet",
+  [chains.base.id]: "base-mainnet",
+  [chains.baseGoerli.id]: "base-goerli",
+  [chains.baseSepolia.id]: "base-sepolia",
+};
+
+export const getRpcHttpUrl = (chainId: number) => {
+  return RPC_CHAIN_NAMES[chainId]
+    ? `https://${RPC_CHAIN_NAMES[chainId]}.g.alchemy.com/v2/${appConfig.alchemyApiKey}`
+    : undefined;
+};
 
 export function getBlockExplorerTxLink(txnHash: string) {
   const targetChain = appConfig.targetNetwork;
